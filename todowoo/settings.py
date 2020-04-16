446980025f646650b838c668a6ed44c80ab3fa5f
TodoWoo <srinathkp1@gmail.com>
2020-04-16 11:50:48 +0000

    Remove old stuff

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^oi0(y@qgf4r^)95hj3k8-w#j)evbs)-b@rd7^6bdb@qguuf4s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todowoo.pythonanywhere.com']
 
 
 # Application definition
@@ -119,4 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 LOGIN_URL = '/login'
